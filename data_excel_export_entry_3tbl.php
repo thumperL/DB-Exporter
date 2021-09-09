86b17dd45e8e3264d7db27f3bf78e6c494be062d
thumperL <shaunwhl1991@gmail.com>
2021-09-09 22:43:20 +1000

    Initialise - The script was created couple years ago for PHP5.6 + MySQL stack. - The script use archived repo PHPExcel to output turn DB to excel spreadsheet

---
+++ data_excel_export_entry_3tbl.php
@@ -0,0 +1,184 @@
+<?php
+// start_tbl 
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+date_default_timezone_set('Australia/Melbourne');
+ini_set('memory_limit', '-1');
+
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
+$sheet_data = array();
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$tables_sql = "SELECT * FROM information_schema.tables a WHERE a.table_schema = '$dbname' ORDER BY a.TABLE_NAME";
+$tables_result = $conn->query($tables_sql);
+
+$conn->select_db($dbname);
+
+
+/* UNCOMMENT ON THE FIRST RUN TO GET THE TABLE NAMES */
+if (isset($_REQUEST['first_run'])) {
+    while ($table_row = $tables_result->fetch_assoc()) {
+        echo '<pre>';
+        var_dump($table_row['TABLE_NAME']);
+        echo '</pre>';
+    }
+    die(__FILE__.__LINE__);
+}
+
+
+// Set the starting table
+if (isset($_REQUEST['start_tbl'])) {
+    $start_table = $_REQUEST['start_tbl'];
+} else {
+    die('SELECT WHICH TABLE TO START WITH....');
+}
+
+if (isset($_REQUEST['batch_size'])) {
+    $batch_size = ((int)$_REQUEST['batch_size'] - 1);
+} else {
+    $batch_size = (int)(3 - 1);
+}
+
+$ittr_count = 0;
+$table_reached = false;
+while ($table_row = $tables_result->fetch_assoc()) {
+    // Start from the starting table
+    if (($table_row['TABLE_NAME'] != $start_table) && ($table_reached == false)) {
+        unset($table_row);
+        continue;
+    } else {
+        $table_reached = true;
+        if ($ittr_count > $batch_size) {
+            unset($table_row);
+            break;
+        } else {
+            // Per table data
+            if ($data = table_data_to_arr($conn, $table_row['TABLE_NAME'])) {
+                $sheet_data[$table_row['TABLE_NAME']] = $data;
+            }
+            unset($data);
+            $ittr_count++;
+        }
+    }
+}
+// Free memory
+mysqli_free_result($tables_result);
+create_xls($dbname, $sheet_data);
+
+
+
+$conn->close();
+die();
+
+
+
+function table_data_to_arr($conn, $sTablename = '')
+{
+    // $conn       -> Connection variable
+    // $sTablename -> Name of Database table from which records need to be exported
+    $data = array();
+
+    // Get table Data
+    $sSelectQry_result = mysqli_query($conn, "SELECT * FROM `$sTablename`");
+
+    if (!$sSelectQry_result) {
+        return;
+    }
+
+    // Heading will be in the first row, loop through all col and get all headings
+    for ($i = 0; $i < $sSelectQry_result->field_count; $i++) {
+        $fieldinfo = $sSelectQry_result->fetch_field_direct($i);
+        $sHeading = $fieldinfo->name;
+        $data[0][] = $sHeading;
+    }
+
+    // Loop through each row
+    $i = 1;
+    while ($sRow = mysqli_fetch_assoc($sSelectQry_result)) //Fetch a result row as an associative array, a numeric array, or both
+    {
+        foreach ($sRow as $ea_val) {
+            $data[$i][] = $ea_val;
+        }
+        $i += 1;
+    }
+    // Free memory
+    mysqli_free_result($sSelectQry_result);
+    unset($fieldinfo);
+    unset($sHeading);
+    unset($sRow);
+
+    return $data;
+}
+
+
+function create_xls($dbname, $data, $table_name = '')
+{
+    /** Include PHPExcel */
+    require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
+
+    // Create new PHPExcel object
+    $objPHPExcel = new PHPExcel();
+
+    // Set document properties
+    $objPHPExcel->getProperties()->setCreator("Shaun Lin")->setTitle("$dbname");
+
+    // Used to set sheet index
+    $sheet_count = 0;
+    foreach ($data as $ea_tbl_name => $ea_tbl_data) {
+        // Add new sheet
+        $objWorkSheet = $objPHPExcel->createSheet($sheet_count);
+
+        // Set all to text, avoid weird data type display
+        $objWorkSheet->getStyle('A1')
+            ->getNumberFormat()
+            ->setFormatCode(
+                PHPExcel_Style_NumberFormat::FORMAT_TEXT
+            );
+
+        // Set first row bold
+        $objWorkSheet->getStyle('1:1')->getFont()->setBold(true);
+
+        // Add data from inner array
+        $objWorkSheet->fromArray($ea_tbl_data, NULL, 'A1');
+
+        // Set sheeet title
+        $objWorkSheet->setTitle("$ea_tbl_name");
+
+        $sheet_count += 1;
+    }
+    
+    // Remove the last empty sheet created
+    $objPHPExcel->removeSheetByIndex($sheet_count);
+    
+    // Redirect output to a client’s web browser (Excel5)
+    header('Content-Type: application/vnd.ms-excel');
+    
+    if ($table_name == '') {
+        header('Content-Disposition: attachment;filename="' . $dbname . '.xls"');
+    } else {
+        header('Content-Disposition: attachment;filename="' . $dbname . '___' . $table_name . '.xls"');
+    }
+    header('Cache-Control: max-age=0');
+    // If you're serving to IE 9, then the following may be needed
+    header('Cache-Control: max-age=1');
+
+    // If you're serving to IE over SSL, then the following may be needed
+    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+    header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+    header('Pragma: public'); // HTTP/1.0
+
+    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+    $objWriter->save('php://output');
+    exit;
+}
